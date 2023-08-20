bc26d9096cb961e63b9b83980d9becef33195a72
sukritx <75582355+sukritx@users.noreply.github.com>
2023-08-20 22:08:07 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>What I've done today 20.8.2023</h1>
+    <ul>
+      <li>Playing Basketball</li>
+      <li>Learn Statistics</li>
+      <li>Learn React</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
